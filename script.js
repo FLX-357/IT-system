8647fb6bb335f26800034d5872079ea4e91d18fe
FLX-357 <stud105965@fh-wedel.de>
2025-01-08 18:39:55 +0100

    1.9

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ document.getElementById("submitTicketButton").addEventListener("click", function
     // Nutzer zur Seite "Meine Tickets" weiterleiten
     window.location.href = "MeineTickets.html";
 });
+const ticketData = JSON.parse(localStorage.getItem("ticketData"));
+    
+if (ticketData) {
+    // Inhalte dynamisch in den Body einfügen
+    document.body.innerHTML = `
+        <h1>Meine Tickets</h1>
+        <p><strong>Vorname:</strong> ${ticketData.firstName}</p>
+        <p><strong>Nachname:</strong> ${ticketData.lastName}</p>
+        <p><strong>Martikelnummer:</strong> ${ticketData.username}</p>
+        <p><strong>Email:</strong> ${ticketData.email}</p>
+        <p><strong>Problem:</strong> ${ticketData.problem}</p>
+    `;
+} else {
+    document.body.innerHTML = "<h1>Keine Tickets gefunden</h1>";
+}
