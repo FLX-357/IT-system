d12b4c8e1a395ca76a10cd15008d269630494ee6
FLX-357 <stud105965@fh-wedel.de>
2025-01-12 19:27:48 +0100

    2.1

---
+++ Stuff.js
@@ -0,0 +1,33 @@
+
+function Test() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+
+
+}
+
+let modalElement = document.querySelector(".modal");
+function Modal() {
+
+
+    modalElement.classList.add("active");
+}
+
+function potato() {
+
+
+    modalElement.classList.remove("active");
+}
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+  document.getElementById("TickTack").innerHTML = date.toLocaleTimeString();
+  document.getElementById("Datum").innerHTML = date.toLocaleDateString();
+}
+ 
+
