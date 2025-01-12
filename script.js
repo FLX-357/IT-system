e66b9583c49d4fa7df2ebca621c7d3a8ea57a7c8
FLX-357 <stud105965@fh-wedel.de>
2025-01-12 20:45:37 +0100

    c

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 document.getElementById("submitTicketButton").addEventListener("click", function () {
-    // Werte aus den Input-Feldern holen
+  
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const problem = document.getElementById("test").value;
 
-    // Daten im LocalStorage speichern
+
     localStorage.setItem("ticketData", JSON.stringify({
         firstName,
         lastName,
@@ -16,6 +16,6 @@ document.getElementById("submitTicketButton").addEventListener("click", function
         problem
     }));
 
-    // Nutzer zur Seite "Meine Tickets" weiterleiten
+  
     window.location.href = "MeineTickets.html";
 });
